c1acffc01a2794a0c4e3c24840756f3a3b913b9d
James Hobin <hobinjk@gmail.com>
2019-01-11 13:35:56 -0500

    Initial commit

---
+++ email-sender-adapter.js
@@ -0,0 +1,162 @@
+const nodemailer = require('nodemailer');
+
+const smtpTransport = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  requireTLS: true,
+  auth: {
+    user: config.email,
+    pass: config.password
+  }
+});
+
+function getMailOptions() {
+  return {
+    from: config.email,
+    to: config.email,
+    subject: '',
+    text: '',
+  };
+}
+
+function sendEmail(to, subject, body) {
+  let transport = createTransport();
+  return new Promise(function(resolve, reject) {
+    transport.sendMail(Object.assign(getMailOptions(), {
+      to: to,
+      subject: subject,
+      text: body,
+    }, function(error, response) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(response);
+      }
+    });
+  });
+}
+
+const emailSenderThing = {
+  type: 'thing',
+  '@context': 'https://iot.mozilla.org/schemas',
+  '@type': [],
+  name: 'Email Sender',
+  properties: [],
+  actions: [
+    {
+      name: 'sendSimple',
+      metadata: {
+        label: 'Send Simple Email',
+        description: 'Send email to yourself with only a subject',
+        input: {
+          type: 'object',
+          properties: {
+            subject: {
+              type: 'string',
+            },
+          },
+        },
+      },
+    },
+    {
+      name: 'send',
+      metadata: {
+        label: 'Send Email',
+        description: 'Send email specifying all details',
+        input: {
+          type: 'object',
+          required: [
+            'to',
+          ],
+          properties: {
+            to: {
+              type: 'string',
+            },
+            subject: {
+              type: 'string',
+            },
+            body: {
+              type: 'string',
+            },
+          },
+        },
+      },
+    },
+  ],
+  events: [],
+};
+
+/**
+ * An email sending device
+ */
+class EmailSenderDevice extends Device {
+  /**
+   * @param {EmailSenderAdapter} adapter
+   * @param {String} id - A globally unique identifier
+   * @param {Object} template - the virtual thing to represent
+   */
+  constructor(adapter, id, template) {
+    super(adapter, id);
+
+    this.name = template.name;
+
+    this.type = template.type;
+    this['@context'] = template['@context'];
+    this['@type'] = template['@type'];
+
+    this.pinRequired = false;
+    this.pinPattern = false;
+
+    for (const action of template.actions) {
+      this.addAction(action.name, action.metadata);
+    }
+
+    for (const event of template.events) {
+      this.addEvent(event.name, event.metadata);
+    }
+
+    this.adapter.handleDeviceAdded(this);
+  }
+
+  async performAction(action) {
+    console.log(`Performing action "${action.name}" with input:`, action.input);
+
+    action.start();
+
+    if (action.name === 'send') {
+      await sendEmail(action.input.to, action.input.subject || '', action.input.body || '');
+    } else if (action.name === 'sendSimple') {
+      await sendEmail(config.email, action.input.subject || 'Notification', '');
+    }
+
+    action.finish();
+  }
+}
+
+/**
+ * Email Sender adapter
+ * Instantiates one email sender device
+ */
+class EmailSenderAdapter extends Adapter {
+  constructor(adapterManager, manifestName) {
+    super(adapterManager, 'email-sender', manifestName);
+
+    adapterManager.addAdapter(this);
+
+    this.addAllThings();
+  }
+
+  startPairing() {
+    this.addAllThings();
+  }
+
+  addAllThings() {
+    if (!this.devices['email-sender-0']) {
+      new EmailSenderDevice(this, 'email-sender-0', emailSenderThing);
+    }
+  }
+}
+
+module.exports = EmailSenderAdapter;
+
